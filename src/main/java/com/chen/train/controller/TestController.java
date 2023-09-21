c3713e78cc03a62d314fe23f8d4fc7275d131f4a
chenwan <3335018910@qq.com>
2023-09-21 19:48:17 +0800

    初始化

---
+++ src/main/java/com/chen/train/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.chen.train.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("/user")
+    public void test(){
+        System.out.println("hello world ");
+
+    }
+}
