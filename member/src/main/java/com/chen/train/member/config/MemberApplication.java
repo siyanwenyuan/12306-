aabc266935c1bfb59a50c184a21f7978a6e0c4f7
chenwan <3335018910@qq.com>
2023-09-22 17:10:29 +0800

    修改启动日志

--- member/src/main/java/com/chen/train/member/config/MemberApplication.java
+++ member/src/main/java/com/chen/train/member/config/MemberApplication.java
@@ -1,9 +1,12 @@
 package com.chen.train.member.config;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,9 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 @ComponentScan("com.chen.train")
 public class MemberApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(MemberApplication.class,args);
+    private static final Logger LOG = LoggerFactory.getLogger(MemberApplication.class);
 
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(MemberApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功！！");
+        LOG.info("测试地址: \thttp://127.0.0.1:{}{}", env.getProperty("server.port"), env.getProperty("server.servlet.context-path"));
     }
 
 
