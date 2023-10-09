ffbd77f058e6f3f6a21334bc52591215a0d4b094
chenwan <3335018910@qq.com>
2023-10-09 19:42:17 +0800

    增加login路由，增加登录页面

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const routes = [
+  //这种加载方式，是项目打包的时候就会被编译，不适合大项目
   {
     path: '/',
     name: 'home',
     component: HomeView
   },
+  //采用懒加载模式，其中懒加载模式表示 在大型项目中当访问当前页面的时候才会被编译
   {
     path: '/about',
     name: 'about',
@@ -14,6 +16,10 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+   {
+    path: '/login',
+    component: () => import( '../views/login.vue')
   }
 ]
 
