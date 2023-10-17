21c9c1f1175d68a3b4132649d59aee3267b0f299
chenwan <3335018910@qq.com>
2023-10-17 16:01:23 +0800

    乘车人编辑接口开发

--- member/src/main/java/com/chen/train/member/service/PassengerService.java
+++ member/src/main/java/com/chen/train/member/service/PassengerService.java
@@ -30,13 +30,22 @@ public class PassengerService {
     public void save(PassengerSaveReq passengerSaveReq) {
         DateTime now = new DateTime().now();
         Passenger passenger = BeanUtil.copyProperties(passengerSaveReq, Passenger.class);
-        //获取本地线程变量中的memberId，而不再需要进行传参设置
-        passenger.setMemberId(LoginMemberContext.getId());
-        passenger.setId(SnowUtil.getSnowflakeNextId());
-        passenger.setCreateTime(now);
-        passenger.setUpdateTime(now);
+        //如果id为空，则说明是新增
+        if(ObjectUtil.isNull(passenger.getId()))
+        {
+            //获取本地线程变量中的memberId，而不再需要进行传参设置
+            passenger.setMemberId(LoginMemberContext.getId());
+            passenger.setId(SnowUtil.getSnowflakeNextId());
+            passenger.setCreateTime(now);
+            passenger.setUpdateTime(now);
+            passengerMapper.insert(passenger);
 
-        passengerMapper.insert(passenger);
+        }else{
+            //否则，则是修改，此时需要加上修改时间
+            passenger.setUpdateTime(now);
+            passengerMapper.updateByPrimaryKey(passenger);
+        }
+
     }
 
     /**
