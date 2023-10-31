50d2770a7fd3f906713e0f017c1c8d79f1e20cc5
chenwan <3335018910@qq.com>
2023-10-31 15:58:53 +0800

    增加quartz定时任务框架

--- batch/src/main/java/com/chen/train/batch/job/springbootTestJob.java
+++ batch/src/main/java/com/chen/train/batch/job/springbootTestJob.java
@@ -9,8 +9,6 @@ import org.springframework.stereotype.Component;
  * 其中自带的定时任务具有缺点
  * 1 只是适合单体应用，不适集群
  * 2 没办法随时修改定时任务的状态
- *
- *
  */
 //此注解表示这个类交给spring管理
 @Component
@@ -20,13 +18,13 @@ public class springbootTestJob {
 
     //此注解表示该类使用什么定时策略，使用cron表达式
     @Scheduled(cron = "0/5 * * * * ?")
-    private void Test(){
+    private void Test() {
         /**
          * 可以加入redis分布式锁解决集群问题
          *    只有当前的拿到锁之后，才可以执行，没有拿到锁，则不可以执行
-         *
          */
         System.out.println("springboot is jobing");
+
     }
 
 
