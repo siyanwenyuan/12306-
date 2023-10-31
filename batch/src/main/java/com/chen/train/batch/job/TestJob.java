f5fcf0daef0281029e057d798784857ee0cc16af
chenwan <3335018910@qq.com>
2023-10-31 16:06:48 +0800

    禁止任务的并发执行

--- batch/src/main/java/com/chen/train/batch/job/TestJob.java
+++ batch/src/main/java/com/chen/train/batch/job/TestJob.java
@@ -1,13 +1,30 @@
 package com.chen.train.batch.job;
 
+import org.quartz.DisallowConcurrentExecution;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
+
+//关闭并发执行
+@DisallowConcurrentExecution
 public class TestJob implements Job {
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-        System.out.println("TestJob is running");
+
+
+        /**
+         * 此如果不关闭任务的并发执行，会出现问题
+         * 因此需要添加注解，关闭任务的并发执行
+         */
+        System.out.println("开始");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("结束");
+
 
     }
 }
